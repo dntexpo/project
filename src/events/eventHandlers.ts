eeff92e92fdb895a7d5cfc516fbb15b98b2ca581
Dante Expo <dntexpo@gmail.com>
2025-04-07 17:23:57 -0300

    feat: delete functionality

--- src/events/eventHandlers.ts
+++ src/events/eventHandlers.ts
@@ -1,12 +1,17 @@
 import { getTodayList, getEventInput } from "../dom/todayList";
 
+type Event = {
+  id: number;
+  text: string;
+};
+
 export function setupEventInput() {
   const todayList = getTodayList();
   const eventInput = getEventInput();
 
   const LOCAL_STORAGE_KEY = "events";
 
-  const getStoredEvents = (): string[] => {
+  const getStoredEvents = (): Event[] => {
     try {
       const raw = localStorage.getItem(LOCAL_STORAGE_KEY);
       return raw ? JSON.parse(raw) : [];
@@ -15,15 +20,31 @@ export function setupEventInput() {
     }
   };
 
-  const saveEvent = (value: string) => {
-    const events = getStoredEvents();
-    events.push(value);
+  const saveEventsToStorage = (events: Event[]) => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(events));
   };
 
-  const renderEvent = (text: string) => {
+  const deleteEvent = (id: number, el: HTMLElement) => {
+    if (!confirm("Are you sure?")) return;
+
+    const events = getStoredEvents().filter((event) => event.id !== id);
+
+    saveEventsToStorage(events);
+
+    el.remove();
+  };
+
+  const renderEvent = ({ id, text }: Event) => {
     const event = document.createElement("span");
+
     event.textContent = text;
+
+    event.title = "Click to delete";
+
+    event.addEventListener("click", () => {
+      deleteEvent(id, event);
+    });
+
     todayList.insertBefore(event, eventInput);
   };
 
@@ -31,9 +52,17 @@ export function setupEventInput() {
     const fragment = document.createDocumentFragment();
     const events = getStoredEvents();
 
-    for (const text of events) {
+    for (const { id, text } of events) {
       const event = document.createElement("span");
+
       event.textContent = text;
+
+      event.title = "Click to delete";
+
+      event.addEventListener("click", () => {
+        deleteEvent(id, event);
+      });
+
       fragment.appendChild(event);
     }
 
@@ -41,11 +70,22 @@ export function setupEventInput() {
   };
 
   const addEvent = () => {
-    const value = eventInput.value.trim();
-    if (!value) return;
+    const text = eventInput.value.trim();
 
-    renderEvent(value);
-    saveEvent(value);
+    if (!text) return;
+
+    const id = Math.max(...getStoredEvents().map((event) => event.id)) + 1;
+
+    const newEvent = { id, text };
+
+    renderEvent(newEvent);
+
+    const events = getStoredEvents();
+
+    events.push(newEvent);
+
+    saveEventsToStorage(events);
+
     eventInput.value = "";
   };
 
